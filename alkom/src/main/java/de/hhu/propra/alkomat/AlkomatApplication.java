baa4bc12cfc9a0c8c2d3ce7004dce427d6b71177
damor103 <53347325+damor103@users.noreply.github.com>
2021-11-11 22:45:12 +0100

    Add files via upload

---
+++ alkom/src/main/java/de/hhu/propra/alkomat/AlkomatApplication.java
@@ -0,0 +1,13 @@
+package de.hhu.propra.alkomat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkomatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlkomatApplication.class, args);
+    }
+
+}
---
+++ alkom/src/test/java/de/hhu/propra/alkomat/AlkomatApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hhu.propra.alkomat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkomatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
